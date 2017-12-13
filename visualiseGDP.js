6f8c08f05c729da1bb1a2ab5d033a1b7a7b59808
Jacky <jackymlui@live.com>
2017-12-14 09:09:41 +1000

    reading in JSON data

--- visualiseGDP.js
+++ visualiseGDP.js
@@ -1,9 +1,15 @@
 var data = [4, 7, 12, 23, 42];
+var gdp = [];
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",(data)=>{
+  gdp = data.data;
+  console.log(gdp);
+})
 
 d3.select(".chart")
 .selectAll("div")
   .data(data)
 .enter().append("div")
-  .style("width", function(d) { return d * 10 + "px"; })
+  // .style("width", function(d) { return 10 + "px"; })
+  .attr('height', function(i){return i*10 + "px";})
   .text(function(d) { return d; });
 
