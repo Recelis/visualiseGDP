299758eb89e51079069ef8447162f49871f2beb6
Jacky <jackymlui@live.com>
2017-12-14 10:54:26 +1000

    getting a bar chart done

--- visualiseGDP.js
+++ visualiseGDP.js
@@ -1,15 +1,39 @@
-var data = [4, 7, 12, 23, 42];
+var data = [4, 7, 12, 23, 42, 20, 150];
 var gdp = [];
-d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",(data)=>{
-  gdp = data.data;
-  console.log(gdp);
-})
 
-d3.select(".chart")
-.selectAll("div")
+
+var chart = d3.select("body")
+  .append('svg')
+  .attr('height',200)
+  .attr('width',200)
+var bars = chart.selectAll('rect')  
   .data(data)
-.enter().append("div")
-  // .style("width", function(d) { return 10 + "px"; })
-  .attr('height', function(i){return i*10 + "px";})
-  .text(function(d) { return d; });
+  .enter().append('rect')
+  .attr('class', "chart")
+  .attr('width', 2)
+  .attr('height', (data)=>{
+    return data;
+  })
+  .attr('x', (d, i) => {
+    console.log(i);
+    return 2 * i;
+  })
+  .attr('fill', 'grey')
+  .attr('y', (d,i)=>{
+    return 200-d
+  })
+  
+
+  console.log(chart);
+// var bars = chart.selectAll('div')  
+// chart.attr('x', (d,i)=>{
+//   return 30*i;
+// })
+
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", (error, data) => {
+  if (error) throw error;
+  gdp = data.data;
+  console.log(gdp); 
+});
+
 
